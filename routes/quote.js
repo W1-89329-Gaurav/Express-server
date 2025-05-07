9012d189f0187b310ad5bd29a95cad2ee35212da
Rahul Bongane <rahulbongane2810@gmail.com>
2025-05-07 12:00:35 +0530

    Api For AddingPost & DeletePostById Functionality

--- routes/quote.js
+++ routes/quote.js
@@ -5,6 +5,7 @@ const express = require("express")
 const bcrypt = require("bcrypt")
 const router = express.Router()
 
+
 router.get("", (req, resp) => {
     db.query("SELECT * FROM quote", (err, results) => {
         if(err)
@@ -23,5 +24,39 @@ router.get("/:id", (req, resp) => {
         return resp.send(apiSuccess(results[0]))
     })
 })
+
+router.post("", (req, resp) => {
+    const {userId, author, contents, createdTime} = req.body
+    db.query("INSERT INTO quote(UserId, author, contents, createdTime) VALUES(?, ? , ? ,NOW())",
+        [userId, author, contents ,createdTime ], (err, result) => {
+            if(err)
+                return resp.send(apiError(err))
+            if(result.affectedRows === 1) {
+                db.query("SELECT * FROM quote WHERE id=?", [result.insertId],
+                    (err, results) => {
+                        if(err)
+                            return resp.send(apiError(err))
+                        resp.send(apiSuccess(results[0]))
+                    }
+                )
+            }
+        }
+    )
+})
+
+router.delete("/:id", (req, resp) => {
+    db.query("DELETE FROM quote WHERE id=?", [req.params.id],
+        (err, result) => {
+            if(err)
+                return resp.send(err)
+            if(result.affectedRows === 1)
+                resp.send(apiSuccess("Quote deleted"))
+            else
+                resp.send(apiError("Quote Record not Deleted"))
+        }
+    )
+})
+
+
 
 module.exports = router
